cce7ead21902547f6e14fc57282b952e65032e6f
Essi Monneus <essimonneus@gmail.com>
2021-05-13 14:42:22 -0400

    Next step is to style it in css

--- apichallenge.js
+++ apichallenge.js
@@ -1,19 +1,9 @@
 let container = document.getElementById("container");
 let list = document.getElementById("list");
 let table = document.getElementById("employees");
-// GET Request.
-// fetch('https://ghibliapi.herokuapp.com/people',{
-//     method: "GET",
-// })
-//     // Handle success
-//     .then(response => response.json())  // convert to json
-//     .then(function(json){
-//         console.log(json)
-//         show(json)
-//     })    //print data to console
-//     .catch(err => console.log('Request Failed', err)); // Catch errors
 
 
+//fetching and getting results from the api
 fetch("https://ghibliapi.herokuapp.com/people")
     .then((res) => res.json())
     .then(function (json) {
@@ -24,33 +14,6 @@ fetch("https://ghibliapi.herokuapp.com/people")
 
 
 // Function to define innerHTML for HTML table
-function show(json) {
-    // let tab =
-    //     `<tr>
-    //       <th>Name</th>
-    //       <th>Gender</th>
-    //       <th>Age</th>
-    //       <th>Eye_color</th>
-    //      </tr>`;
-
-    // Loop to access all rows 
-    for (let r of json) {
-        console.log(r.name)
-        // let row = document.createElement("tr")
-        // let columnName = document.createElement("td")
-        // columnName.innerHTML = r.name
-        // table.appendChild(row)
-        // row.appendChild(columnName)
-        //    tab += `<tr> 
-        //<td>${r.name} </td>
-        //<td>${r.gender}</td>
-        //<td>${r.age}</td> 
-        //<td>${r.eye_color}</td>          
-        //</tr>`;
-    }
-    // Setting innerHTML as tab variable
-}
-
 function display(json) {
     for (r of json) {
         console.log(r.name)
